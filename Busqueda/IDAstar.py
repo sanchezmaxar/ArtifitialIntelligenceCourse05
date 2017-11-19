e485df687f8bcd2ce777fa5f6042b9aaf0460159
Edgar <xeicker@gmail.com>
2017-11-18 20:42:03 -0600

    IDA*

---
+++ Busqueda/IDAstar.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from collections import deque
+from utils import *
+from puzzle import *
+from manhattan import *
+
+class IDAstar:
+    @staticmethod
+    def search(origen,stop,maxdepth,heuristica):
+        if stop(origen):
+            return ruta(origen)
+        frontera=1+heuristica(origen)
+        while frontera<=maxdepth:
+            ac=0
+            aux=0
+            agenda = deque()
+            expandidos = set()
+            agenda.appendleft(origen)
+            while agenda:
+                nodo = agenda.popleft()
+                expandidos.add(nodo)
+                if(profundidad(nodo)<=frontera):
+                    for hijo in nodo.expand():
+                        if stop(hijo):
+                            return ruta(hijo)
+                        if hijo not in expandidos:
+                            agenda.appendleft(hijo)
+                x = profundidad(nodo)+heuristica(nodo)
+                if ac==0:
+                    aux=x
+                    ac=1
+                elif aux>x and x>frontera:
+                    aux=x
+            if aux <= frontera:
+                frontera+=1
+            else:
+                frontera=aux
+            print frontera
+tablero = Puzzle()
+print IDAstar().search(tablero.shuffle(20),lambda x: x == Puzzle(),20,ManhattanDistance().distance_to_target)
